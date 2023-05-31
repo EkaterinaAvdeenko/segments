248164d51b630c11077db0cca182b05475beffca
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-05-31 13:25:12 +0200

    segments_AvdeenkEkaterina

--- src/main/java/com/epam/rd/autotasks/segments/Point.java
+++ src/main/java/com/epam/rd/autotasks/segments/Point.java
@@ -3,12 +3,21 @@ package com.epam.rd.autotasks.segments;
 class Point {
     private double x;
     private double y;
+    Point start;
+    Point end;
+
 
     public Point(final double x, final double y) {
         this.x = x;
         this.y = y;
     }
-
+    @Override
+    public boolean equals (Object other) {
+        if (this == other) return true;
+        if (other == null || !(other instanceof Point)) return false;
+        Point p = (Point) other;
+        return x == p.x && y == p.y;
+    }
     public double getX() {
         return x;
     }
@@ -16,4 +25,13 @@ class Point {
     public double getY() {
         return y;
     }
-}
+    }
+
+
+
+
+
+
+
+
+
