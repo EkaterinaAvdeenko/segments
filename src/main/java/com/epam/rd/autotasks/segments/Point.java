acca232e6a50271a1ca3e2f34691e14a5340756a
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 20:29:37 +0100

    segments_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/segments/Point.java
+++ src/main/java/com/epam/rd/autotasks/segments/Point.java
@@ -1,31 +1,38 @@
 package com.epam.rd.autotasks.segments;
 
 class Point {
-    private double x;
-    private double y;
-    Point start;
-    Point end;
+
+  Point start;
+  Point end;
+  private final double x;
+  private final double y;
 
 
-    public Point(final double x, final double y) {
-        this.x = x;
-        this.y = y;
-    }
-    @Override
-    public boolean equals (Object other) {
-        if (this == other) return true;
-        if (other == null || !(other instanceof Point)) return false;
-        Point p = (Point) other;
-        return x == p.x && y == p.y;
-    }
-    public double getX() {
-        return x;
-    }
+  public Point(final double x, final double y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    public double getY() {
-        return y;
-    }
-    }
+  @Override
+  public boolean equals(Object other) {
+      if (this == other) {
+          return true;
+      }
+      if (other == null || !(other instanceof Point)) {
+          return false;
+      }
+    Point p = (Point) other;
+    return x == p.x && y == p.y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+}
 
 
 
--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -3,48 +3,63 @@ package com.epam.rd.autotasks.segments;
 import java.util.Objects;
 
 import static java.lang.Math.abs;
+
 class Segment {
-     Point start;
-     Point end;
-     public Segment (Point start, Point end) {
-        if (start == null || end == null)
-            throw new IllegalArgumentException("Arguments can't be null");
-         if (start.equals(end))
-             throw new IllegalArgumentException("The points must differ");
-        this.start = start;
-        this.end = end;
+
+  Point start;
+  Point end;
+
+  public Segment(Point start, Point end) {
+    if (start == null || end == null) {
+      throw new IllegalArgumentException("Arguments can't be null");
     }
+    if (start.equals(end)) {
+      throw new IllegalArgumentException("The points must differ");
+    }
+    this.start = start;
+    this.end = end;
+  }
 
   double length() {
-        double xDistanceSquare = Math.pow(end.getX() - start.getX(), 2);
-        double yDistanceSquare = Math.pow(end.getY() - start.getY(), 2);
-        return Math.sqrt(xDistanceSquare + yDistanceSquare);
-   }
+    double xDistanceSquare = Math.pow(end.getX() - start.getX(), 2);
+    double yDistanceSquare = Math.pow(end.getY() - start.getY(), 2);
+    return Math.sqrt(xDistanceSquare + yDistanceSquare);
+  }
 
-    Point middle() {
-        return new Point( (start.getX() + end.getX()) / 2,
-                (start.getY() + end.getY()) / 2 );
+  Point middle() {
+    return new Point((start.getX() + end.getX()) / 2,
+        (start.getY() + end.getY()) / 2);
+  }
+
+  Point intersection(Segment another) {
+    double k1 = (end.getY() - start.getY()) / (end.getX() - start.getX());
+    double k2 =
+        (another.end.getY() - another.start.getY()) / (another.end.getX() - another.start.getX());
+    if (k1 == k2) {
+      return null;
     }
 
-    Point intersection(Segment another) {
-        double k1 = (end.getY() - start.getY()) / (end.getX() - start.getX());
-        double k2 = (another.end.getY() - another.start.getY()) / (another.end.getX() - another.start.getX());
-        if (k1 == k2) return null;
+    double b1 =
+        (start.getY() * end.getX() - end.getY() * start.getX()) / (end.getX() - start.getX());
+    double b2 =
+        (another.start.getY() * another.end.getX() - another.end.getY() * another.start.getX()) /
+            (another.end.getX() - another.start.getX());
 
-        double b1 = (start.getY() * end.getX() - end.getY() * start.getX()) / (end.getX() - start.getX());
-        double b2 = (another.start.getY() * another.end.getX() - another.end.getY() * another.start.getX()) /
-                (another.end.getX() - another.start.getX());
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * b2 - k2 * b1) / (k1 - k2);
 
-        double x = (b2 - b1) / (k1 - k2);
-        double y = (k1 * b2 - k2 * b1) / (k1 - k2);
-
-        if ((x > start.getX() && x > end.getX()) || (x > another.start.getX() && x > another.end.getX()) ||
-                (x < start.getX() && x < end.getX()) || (x < another.start.getX() && x < another.end.getX()) ||
-                (y > start.getY() && y > end.getY()) || (y > another.start.getY() && y > another.end.getY()) ||
-                (y < start.getY() && y < end.getY()) || (y < another.start.getY() && y < another.end.getY()))
-            return null;
-
-        return new Point((double) x, (double) y);
+    if ((x > start.getX() && x > end.getX()) || (x > another.start.getX() && x > another.end.getX())
+        ||
+        (x < start.getX() && x < end.getX()) || (x < another.start.getX() && x < another.end.getX())
+        ||
+        (y > start.getY() && y > end.getY()) || (y > another.start.getY() && y > another.end.getY())
+        ||
+        (y < start.getY() && y < end.getY()) || (y < another.start.getY()
+        && y < another.end.getY())) {
+      return null;
     }
+
+    return new Point(x, y);
+  }
 }
 
